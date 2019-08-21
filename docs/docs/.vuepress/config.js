9b946a8c705bb8c4b42b314f95745284cb68efcf
Andrew Welch <andrew@nystudio107.com>
2019-08-21 16:06:59 -0400

    Move docs to sub-folder
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: 'Eager Beaver Documentation',
     description: 'Documentation for the Eager Beaver plugin',
     base: '/docs/eager-beaver/',
-    dest: '../../../sites/nystudio107/web/docs/eager-beaver',
+    dest: '../../../../sites/nystudio107/web/docs/eager-beaver',
     themeConfig: {
         sidebar: [
             ['/', 'Eager Beaver plugin for Craft CMS 3.x'],
